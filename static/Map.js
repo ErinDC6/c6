dfc291dd280f0322ee1d537313293fe226584c51
James Prow <jkprow@gmail.com>
2019-12-13 11:46:05 -0800

    remove # from addresses

--- static/Map.js
+++ static/Map.js
@@ -1,9 +1,10 @@
 import { h } from './preact.js';
 
 export default function Map({ address }) {
+  const sanitizedAddress = address.replace('#', '');
   return h(
     'iframe',
-    { src: `https://maps.google.com/maps?q=${address}&t=&z=13&ie=UTF8&iwloc=&output=embed` }
+    { src: `https://maps.google.com/maps?q=${sanitizedAddress}&t=&z=13&ie=UTF8&iwloc=&output=embed` }
   )
 }
 
